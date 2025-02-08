418430152f174a886f6ba68133297a2735526366
Luis Henrique Santana Paiva <166076350+LuisHenriqueSantana@users.noreply.github.com>
2025-02-08 19:19:29 -0300

    Create acess.js

---
+++ acess.js
@@ -0,0 +1,14 @@
+(function() {
+  const dominiosPermitidos = [
+https://rikbranding.catalog.yampi.io/
+  ];
+
+  const domainAtual = window.location.hostname;
+  
+  if (dominiosPermitidos.includes(domainAtual)) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://seu-usuario.github.io/meu-css-cdn/css/style.min.css';
+    document.head.appendChild(link);
+  }
+})();
