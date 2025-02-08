ebad703d3958ac6dc21cb4e4eb76707dde567dfe
Luis Henrique Santana Paiva <166076350+LuisHenriqueSantana@users.noreply.github.com>
2025-02-08 19:21:27 -0300

    Update acess.js

--- acess.js
+++ acess.js
@@ -1,14 +1,15 @@
 (function() {
-  const dominiosPermitidos = [
-https://rikbranding.catalog.yampi.io/
-  ];
+  const sitesPermitidos = {
+    'https://rikbranding.catalog.yampi.io/': 'chave123',
+  };
 
   const domainAtual = window.location.hostname;
+  const chaveInformada = document.currentScript.getAttribute('data-key');
   
-  if (dominiosPermitidos.includes(domainAtual)) {
+  if (sitesPermitidos[domainAtual] === chaveInformada) {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'https://LuisHenriqueSantana.github.io/meu-css-cdn/css/style.min.css';
     document.head.appendChild(link);
   }
-})();
+})(); 
